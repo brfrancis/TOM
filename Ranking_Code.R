fcc45e06022c34e9ab1b6e37117eba7531924c50
brfrancis <b.r.francis@outlook.com>
2018-07-02 16:50:49 +0100

    Fixed chip time issue

--- Ranking_Code.R
+++ Ranking_Code.R
@@ -1,17 +1,9 @@
-install.packages("rvest")#
-install.packages("selectorgadget")
+#install.packages("rvest")#
+#install.packages("selectorgadget")
 require(rvest)
 require(XML)
 require(googlesheets)
 require(chron)
-
-
-get_pos <- function(html){
-  html %>% 
-    # The relevant tag
-    html_nodes('.tbody') %>%
-    html_text
-}
 
 pg<-1
 url<-paste('https://www.resultsbase.net/event/4509/results?page=',pg,sep="")
@@ -23,6 +15,7 @@ bib<-as.numeric(population[[1]]$`Bibno.`[seq(1, 250, 5)])
 name<-population[[1]]$`Participant`[seq(1, 250, 5)]
 chip<-population[[1]]$`Chip time`[seq(1, 250, 5)]
 gun<-population[[1]]$`Finish time`[seq(1, 250, 5)]
+gen<-population[[1]]$`Gender`[seq(1, 250, 5)]
 
 
 for(pg in seq(2,32)){
@@ -40,8 +33,8 @@ for(pg in seq(2,32)){
 
 res<-cbind(as.numeric(bib),name,gun,chip)
 tom_res1<-res[as.numeric(res[,1])>6000&!is.na(res[,1]),]
-tom_res=cbind(order(60*24*times(tom_res1[,3])),order(60*24*times(tom_res1[,4])),tom_res1)
-colnames(tom_res)<-c("Tour Position (Gun)","Position (Chip)", "Bib", "Name", "Chip Time", "Gun Time")
+tom_res=cbind(order(60*24*times(tom_res1[,3])),order(order(60*24*times(tom_res1[,4]))),tom_res1)
+colnames(tom_res)<-c("Tour Position (Gun)","Position (Chip)", "Bib", "Name", "Gun Time", "Chip Time")
 tom_res %>%
 write.csv("TOM_Southport.csv", row.names = FALSE)
 TOM_Southport <- gs_upload("TOM_Southport.csv",overwrite = TRUE) 
